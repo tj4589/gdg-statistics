d9c72a3abc75e0a315bd6deab23ec950191858b1
Tjaey Lee <ekpokpobeoghenetejiri29@gmail.com>
2025-10-24 23:39:56 +0100

    testing testing 2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 // Load environment variables
 dotenv.config();
 
