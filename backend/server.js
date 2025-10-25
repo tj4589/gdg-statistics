b7a7763bff4eaee5e96f46a2058d42a14af00e5c
Tjaey Lee <ekpokpobeoghenetejiri29@gmail.com>
2025-10-25 22:49:00 +0100

    scors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://gdg-registration-form.netlify.app" // Or '*' for all origins
+    "*" // Or '*' for all origins
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
